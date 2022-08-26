a50f597e0fb85c8e8193354e75cd2303f8385be3
Serhii <kamforovich@gmail.com>
2022-08-26 19:01:27 +0300

    upd

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,14 @@
+
+  let burger = document.querySelector(".burger-button");
+  let mainMenu = document.querySelector(".header__menu");
+
+  burger.addEventListener("click", toggleactive);
+
+  function toggleactive() {
+    burger.classList.toggle("active");
+    mainMenu.classList.toggle("active");
+  }
+
 (() => {
     const refs = {
       openModalBtn: document.querySelector("[data-modal-open]"),
